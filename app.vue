c27c7605b2f99c623d81df9019972f90280b78ca
y-sano <mmyy_125115@gmail.com>
2022-04-16 00:12:29 +0900

    Composition API test

--- app.vue
+++ app.vue
@@ -1,5 +1,36 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <div>
+      <input v-model="name.last"><input v-model="name.first">
+    </div>
+    <div>名前：{{ fullName }}</div>
+    <button @click.prevent="doGreet">あいさつ</button>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, reactive, computed } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const name = reactive({
+      first: '太郎',
+      last: '山田'
+    })
+
+    const fullName = computed(() => {
+      return `${name.last} ${name.first}`
+    })
+
+    const doGreet = () => {
+      console.log(`こんにちは、${fullName.value}さん`)
+    }
+
+    return {
+      name,
+      fullName,
+      doGreet
+    }
+  },
+})
+</script>
